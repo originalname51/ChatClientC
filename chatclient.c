a52083fa22cf2fe789b4ba6fe4f87c76e2ea3e2b
rob <robwstine@outlook.com>
2016-07-19 20:48:04 -0400

    set up the socket and get it ready to connect to the server.

--- chatclient.c
+++ chatclient.c
@@ -8,7 +8,28 @@
 
 int main(int argc, char *argv[])
 {
-	printf("hello there");
-    return 0;
+	int sockfd, portno;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
+
+	portno = atoi(argv[2]);
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+		perror("ERROR opening socket");
+	server = gethostbyname(argv[1]);
+
+	if (server == NULL) {
+		fprintf(stderr, "ERROR, no such host\n");
+		exit(0);
+	}
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	bcopy((char *) server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+	serv_addr.sin_port = htons(portno);
+	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+		perror("ERROR connecting");
+
+	return 0;
 }
 
--- chatclient.h
+++ chatclient.h
@@ -8,7 +8,15 @@
 #ifndef CHATCLIENT_H_
 #define CHATCLIENT_H_
 
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include  <fcntl.h>
+
 
 #endif /* CHATCLIENT_H_ */
